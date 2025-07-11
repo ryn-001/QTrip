b32c999de99d565804546342ff6593ce5b53bcc3
schizo <schizo69mars@gmail.com>
2025-07-11 22:23:05 +0530

    api fetched

---
+++ frontend/modules/landing_page.js
@@ -0,0 +1,39 @@
+import config from '../conf/index.js';
+
+async function init(){
+    const data = await fetchCities();
+
+    data.forEach((city) => {
+        addCities(city.id,city.city,city.description,city.image);
+    });
+}
+
+async function fetchCities(){
+    try{
+        const response = await fetch(`${config.backendpoint}/cities`);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }catch{
+        return null;
+    }  
+}
+
+function addCities(id,name,description,image){
+    const cities = document.getElementById("cities");
+    
+    const cityCard = document.createElement("div");
+    cityCard.setAttribute('class','city-card');
+    cityCard.id = id;
+    
+    const img = document.createElement('img');
+    if(id == 'singapore') img.src = 'https://media.istockphoto.com/id/150451379/photo/singapore-city-skyline-at-night.jpg?s=612x612&w=0&k=20&c=usTn9W-kDOlU2uaCrP7TQPMCp1O5tljUT1__BRg5VRg='
+    else img.src = image;
+
+    cityCard.appendChild(img);
+    
+
+    cities.append(cityCard);
+}
+
+init();
