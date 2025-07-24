35f42a1a939684211efef5bb77776e3c4557c513
schizo <schizo69mars@gmail.com>
2025-07-25 00:17:02 +0530

    Adding stuff to adventures pages

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -27,7 +27,7 @@ function addCities(id,name,description,image){
     cityCard.id = id;
     
     const anchor = document.createElement('a');
-    anchor.href = 'adventures.html';
+    anchor.href = `/frontend/pages/adventures/adventures.html?city=${id}`;
 
     const img = document.createElement('img');
     if(id == 'singapore') img.src = 'https://media.istockphoto.com/id/150451379/photo/singapore-city-skyline-at-night.jpg?s=612x612&w=0&k=20&c=usTn9W-kDOlU2uaCrP7TQPMCp1O5tljUT1__BRg5VRg='
