104738bb4c4627ab98d49570ff6d235f3bb02656
schizo <aryan.khaire001@gmail.com>
2025-08-08 00:03:20 +0530

    adding adventures to DOM

--- modules/adventures_page.js
+++ modules/adventures_page.js
@@ -1,6 +1,10 @@
 import config from '../config/config.js'
 
+let adventures = null;
+
 const data = document.getElementsByClassName("row")[0];
+const durationClear = document.getElementsByClassName("duration-clear")[0];
+const categoryClear = document.getElementsByClassName("category-clear")[0];
 
 async function fetchAdventuresCity(URL){
     let params = new URLSearchParams(URL);
@@ -17,10 +21,19 @@ async function fetchAdventuresData(city){
     }
 }
 
+function addAdventureToDOM(adventures){
+    adventures.forEach((data) => {
+        const card = document.createElement("div");
+        card.className = "card col-12 col-sm-2 col-md-4 col-lg-3";
+
+    })
+}
+
 
 async function init(){
     let city = await fetchAdventuresCity(window.location.search);
-    let adventuresCityData = await fetchAdventuresData(city);
-    console.log(adventuresCityData);
+    adventures = await fetchAdventuresData(city);
+    console.log(adventures);
+    addAdventureToDOM(adventures);
 }
 init();
